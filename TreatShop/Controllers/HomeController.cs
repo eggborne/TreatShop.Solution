ca008d80d8e7c18e506a9999bc3ae3d2b9b597aa
Mike Donovan <mike@mikedonovan.dev>
2023-03-24 11:36:00 -0700

    Create Treats index, move modal locations

--- TreatShop/Controllers/HomeController.cs
+++ TreatShop/Controllers/HomeController.cs
@@ -1,23 +1,32 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Linq;
 using TreatShop.Models;
 
 namespace TreatShop.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly TreatShopContext _db;
+
+      public HomeController(TreatShopContext db)
+      {
+        _db = db;
+      }
 
       [HttpGet("/")]
       public ActionResult Index()
       {
+        ViewBag.Treats = _db.Treats
+                      .Include(treat => treat.JoinEntities)
+                      .ThenInclude(join => join.Flavor)
+                      .ToList();
+        ViewBag.Flavors = _db.Flavors
+                      .Include(flavor => flavor.JoinEntities)
+                      .ThenInclude(join => join.Treat)
+                      .ToList();
         return View();
       }
-
-      [HttpPost()]
-      public ActionResult Index(ApplicationUser applicationUser)
-      {
-        return View();
-      }
-
     }
 }
---
+++ TreatShop/Controllers/TreatsController.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using TreatShop.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TreatShop.Controllers
+{
+  public class TreatsController : Controller
+  {
+    private readonly TreatShopContext _db;
+
+    public TreatsController(TreatShopContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Treat> model = _db.Treats.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Treat treat)
+    {
+      _db.Treats.Add(treat);
+
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Treat thisTreat = _db.Treats
+                        .FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Treat treat)
+    {
+      _db.Treats.Update(treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      _db.Treats.Remove(thisTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
