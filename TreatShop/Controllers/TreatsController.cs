a66dbf182c3a8081b14e9816ae37db966c8f69df
Mike Donovan <mike@mikedonovan.dev>
2023-03-24 17:10:28 -0700

    Implement Edit function for Treat

--- TreatShop/Controllers/TreatsController.cs
+++ TreatShop/Controllers/TreatsController.cs
@@ -46,18 +46,18 @@ namespace TreatShop.Controllers
       return View(thisTreat);
     }
 
-    public ActionResult Edit(int id)
-    {
-      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
-      return View(thisTreat);
-    }
+    // public ActionResult Edit(int id)
+    // {
+    //   Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+    //   return View(thisTreat);
+    // }
 
     [HttpPost]
     public ActionResult Edit(Treat treat)
     {
       _db.Treats.Update(treat);
       _db.SaveChanges();
-      return RedirectToAction("Index");
+      return RedirectToAction("Details", new { id = treat.TreatId });
     }
 
     [HttpPost]
